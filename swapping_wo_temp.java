60918529f97dd70d6457fd936fc69f608b65a6b2
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-25 19:18:34 +0530

    Create swapping_wo_temp.java

---
+++ swapping_wo_temp.java
@@ -0,0 +1,16 @@
+//java program to swap the two numbers without using temporary variable
+
+import java.util.Scanner;
+class swap2
+{
+
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+ int i,a,b,t;
+ System.out.println("ENTER THE NUMBERS");
+a=in.nextInt();
+b=in.nextInt();
+System.out.println("THE NUMBERS BEFORE SWAPPING ARE  a = " + a + " b = "  + b); 
+System.out.println("THE NUMBERS AFTER SWAPPING ARE  a = " + b + " b = "  + a); 
+}}
