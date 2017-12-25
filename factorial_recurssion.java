96a02b470f902316d09b6aa341382e5d467a4b5f
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2017-12-25 18:55:33 +0530

    Create factorial_recurssion.java

---
+++ factorial_recurssion.java
@@ -0,0 +1,17 @@
+//java program to find the factorial of the given integer using recurssion
+
+import java.util.Scanner;
+class fact
+{
+public static void main(String args[])
+{
+Scanner in=new Scanner(System.in);
+ int n,fact=1,i;
+ System.out.println("ENTER THE INTEGER");
+n=in.nextInt();
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+System.out.println("THE FACTORIAL OF GIVEN INTEGER IS : "  +  fact);
+}}
