082ee57c00efc1c80adeac3dd9f5f63c5116ce87
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-02-08 10:30:05 +0530

    Create exception1.java

---
+++ exception1.java
@@ -0,0 +1,18 @@
+package sample;
+import java.util.Scanner;
+public class division {
+	public static void main(String[] args) {
+		 
+		  int a, b, result;
+		 
+		    System.out.println("Input two integers");
+		    Scanner input=new Scanner(System.in);
+		 
+		  a = input.nextInt();
+		  b = input.nextInt();
+		 
+		  result = a / b;
+		 
+		  System.out.println("Result = " + result);
+		  }
+		}
