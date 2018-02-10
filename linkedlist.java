201e35c4066313395350b73af540eed6500bd0c3
RAMYAMURUGESH <ramyamurugesh333@gmail.com>
2018-02-10 10:02:07 +0530

    Create linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,31 @@
+package student;
+class node
+{
+	int data;
+	node next;
+	node(int a)
+	{
+		data=a;
+		next=null;
+	}
+}
+public class linked_list {
+	 
+	public static void main(String args[])
+	{
+		node head,first,sec,temp;
+		head=new node(10);
+		first=new node(20);
+		sec=new node(30);
+		head.next=first;
+		first.next=sec;
+		sec.next=null;
+		temp=head;
+		while(temp!=null)
+		{
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+	}
+
+}
